6ad73720a05f4d0721fe6dc0a6aaf0478afbe350
Rajawiw790 <108253254+Rajawiw790@users.noreply.github.com>
2026-07-09 04:15:56 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,29 +1,30 @@
 const Discord = require('discord.js');
 const chalk = require('chalk');
-require('dotenv').config('./.env');
+// مسحنا dotenv حيت ماشي محتاجينو فـ Railway
+// require('dotenv').config('./.env');
 const axios = require('axios');
+
 // Check if is up to date
 const { version } = require('.././package.json');
 axios.get('https://api.github.com/repos/CorwinDev/Discord-Bot/releases/latest').then(res => {
-    if (res.data.tag_name !== version) {
+    if (res.data.tag_name!== version) {
         console.log(chalk.red.bgYellow(`Your bot is not up to date! Please update to the latest version!`, version + ' -> ' + res.data.tag_name));
     }
 }).catch(err => {
     console.log(chalk.red.bgYellow(`Failed to check if bot is up to date!`));
 });
 
-
 const webhook = require("./config/webhooks.json");
 const config = require("./config/bot.js");
 const webHooksArray = ['startLogs', 'shardLogs', 'errorLogs', 'dmLogs', 'voiceLogs', 'serverLogs', 'serverLogs2', 'commandLogs', 'consoleLogs', 'warnLogs', 'voiceErrorLogs', 'creditLogs', 'evalLogs', 'interactionLogs'];
-// Check if .env webhook_id and webhook_token are set
+
+// Check if.env webhook_id and webhook_token are set
 if (process.env.WEBHOOK_ID && process.env.WEBHOOK_TOKEN) {
     for (const webhookName of webHooksArray) {
         webhook[webhookName].id = process.env.WEBHOOK_ID;
         webhook[webhookName].token = process.env.WEBHOOK_TOKEN;
     }
 }
-
 
 const startLogs = new Discord.WebhookClient({
     id: webhook.startLogs.id,
@@ -37,9 +38,10 @@ const shardLogs = new Discord.WebhookClient({
 
 const manager = new Discord.ShardingManager('./src/bot.js', {
     totalShards: 'auto',
-    token: process.env.DISCORD_TOKEN,
+    token: process.env.DISCORD_TOKEN, // <--- هنا كيقرا من Railway
     respawn: true
 });
+
 if (process.env.TOPGG_TOKEN) {
     const { AutoPoster } = require('topgg-autoposter');
     AutoPoster(process.env.TOPGG_TOKEN, manager);
@@ -56,9 +58,9 @@ console.log(`\u001b[0m`);
 
 manager.on('shardCreate', shard => {
     let embed = new Discord.EmbedBuilder()
-        .setTitle(`🆙・Launching shard`)
-        .setDescription(`A shard has just been launched`)
-        .setFields([
+       .setTitle(`🆙・Launching shard`)
+       .setDescription(`A shard has just been launched`)
+       .setFields([
             {
                 name: "🆔┆ID",
                 value: `${shard.id + 1}/${manager.totalShards}`,
@@ -70,7 +72,7 @@ manager.on('shardCreate', shard => {
                 inline: true
             }
         ])
-        .setColor(config.colors.normal)
+       .setColor(config.colors.normal)
     startLogs.send({
         username: 'Bot Logs',
         embeds: [embed],
@@ -81,14 +83,14 @@ manager.on('shardCreate', shard => {
 
     shard.on("death", (process) => {
         const embed = new Discord.EmbedBuilder()
-            .setTitle(`🚨・Closing shard ${shard.id + 1}/${manager.totalShards} unexpectedly`)
-            .setFields([
+           .setTitle(`🚨・Closing shard ${shard.id + 1}/${manager.totalShards} unexpectedly`)
+           .setFields([
                 {
                     name: "🆔┆ID",
                     value: `${shard.id + 1}/${manager.totalShards}`,
                 },
             ])
-            .setColor(config.colors.normal)
+           .setColor(config.colors.normal)
         shardLogs.send({
             username: 'Bot Logs',
             embeds: [embed]
@@ -96,8 +98,8 @@ manager.on('shardCreate', shard => {
 
         if (process.exitCode === null) {
             const embed = new Discord.EmbedBuilder()
-                .setTitle(`🚨・Shard ${shard.id + 1}/${manager.totalShards} exited with NULL error code!`)
-                .setFields([
+               .setTitle(`🚨・Shard ${shard.id + 1}/${manager.totalShards} exited with NULL error code!`)
+               .setFields([
                     {
                         name: "PID",
                         value: `\`${process.pid}\``,
@@ -107,7 +109,7 @@ manager.on('shardCreate', shard => {
                         value: `\`${process.exitCode}\``,
                     }
                 ])
-                .setColor(config.colors.normal)
+               .setColor(config.colors.normal)
             shardLogs.send({
                 username: 'Bot Logs',
                 embeds: [embed]
@@ -117,9 +119,9 @@ manager.on('shardCreate', shard => {
 
     shard.on("shardDisconnect", (event) => {
         const embed = new Discord.EmbedBuilder()
-            .setTitle(`🚨・Shard ${shard.id + 1}/${manager.totalShards} disconnected`)
-            .setDescription("Dumping socket close event...")
-            .setColor(config.colors.normal)
+           .setTitle(`🚨・Shard ${shard.id + 1}/${manager.totalShards} disconnected`)
+           .setDescription("Dumping socket close event...")
+           .setColor(config.colors.normal)
         shardLogs.send({
             username: 'Bot Logs',
             embeds: [embed],
@@ -128,8 +130,8 @@ manager.on('shardCreate', shard => {
 
     shard.on("shardReconnecting", () => {
         const embed = new Discord.EmbedBuilder()
-            .setTitle(`🚨・Reconnecting shard ${shard.id + 1}/${manager.totalShards}`)
-            .setColor(config.colors.normal)
+           .setTitle(`🚨・Reconnecting shard ${shard.id + 1}/${manager.totalShards}`)
+           .setColor(config.colors.normal)
         shardLogs.send({
             username: 'Bot Logs',
             embeds: [embed],
@@ -137,9 +139,7 @@ manager.on('shardCreate', shard => {
     });
 });
 
-
 manager.spawn();
-
 
 // Webhooks
 const consoleLogs = new Discord.WebhookClient({
@@ -154,19 +154,20 @@ const warnLogs = new Discord.WebhookClient({
 
 process.on('unhandledRejection', error => {
     console.error('Unhandled promise rejection:', error);
-    if (error) if (error.length > 950) error = error.slice(0, 950) + '... view console for details';
-    if (error.stack) if (error.stack.length > 950) error.stack = error.stack.slice(0, 950) + '... view console for details';
+    let errMsg = error;
+    if (error) if (error.length > 950) errMsg = error.slice(0, 950) + '... view console for details';
+    if (error.stack) if (error.stack.length > 950) errMsg = error.stack.slice(0, 950) + '... view console for details';
     if (!error.stack) return
     const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・Unhandled promise rejection`)
-        .addFields([
+       .setTitle(`🚨・Unhandled promise rejection`)
+       .addFields([
             {
                 name: "Error",
-                value: error ? Discord.codeBlock(error) : "No error",
+                value: errMsg? Discord.codeBlock(errMsg) : "No error",
             },
             {
                 name: "Stack error",
-                value: error.stack ? Discord.codeBlock(error.stack) : "No stack error",
+                value: error.stack? Discord.codeBlock(error.stack) : "No stack error",
             }
         ])
     consoleLogs.send({
@@ -181,11 +182,11 @@ process.on('unhandledRejection', error => {
 process.on('warning', warn => {
     console.warn("Warning:", warn);
     const embed = new Discord.EmbedBuilder()
-        .setTitle(`🚨・New warning found`)
-        .addFields([
+       .setTitle(`🚨・New warning found`)
+       .addFields([
             {
                 name: `Warn`,
-                value: `\`\`\`${warn}\`\`\``,
+                value: `\`\`\`${warn}\`\``,
             },
         ])
     warnLogs.send({
